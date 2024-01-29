3262ea5d141784678562734eeae51346d9ef1fdb
Azamatov Iznaur <azamatoviznaur@gmail.com>
2024-01-29 22:59:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,28 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 const root = document.getElementById('root');
 
-const reducer = (state, action) => {
-    if (action.type === 'PLUS') {
-        return state + 1;
-    }
-    if (action.type === 'MINUS') {
-        return state - 1;
-    }
-    if (action.type === 'RESET') {
-        return state = 0;
-    }
-    return 0;
-}
-
-const store = createStore(reducer);
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+
+        <App/>,
+
     root
 );
